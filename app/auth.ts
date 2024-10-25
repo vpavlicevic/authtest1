7a688f2742d784e490d0158530889d12f2bd00eb
Vuk <vuk.pavlicevic.21@singimail.rs>
2024-10-25 18:34:35 +0200

    Role adding #1

--- app/auth.ts
+++ app/auth.ts
@@ -21,4 +21,11 @@ export const {
       },
     }),
   ],
+  callbacks: {
+    session({ session, user }) {
+      // @ts-ignore
+      session.user.role = user.role
+      return session
+    },
+  }
 });
